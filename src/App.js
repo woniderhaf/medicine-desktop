a98df2d44fa43f7faf905a7272dc343d6ce698b9
woniderhaf <physics56@bk.ru>
2023-02-06 09:40:55 +0300

    fix

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 // import socket from './socket/socket';
-import {Routes,Route} from 'react-router-dom'
+import {Routes,Route, Navigate} from 'react-router-dom'
 import CreateRoom from './pages/CreateRoom/Index';
 import Room from './pages/Room/Index';
 function App() {
@@ -9,7 +9,10 @@ function App() {
       <Routes>
         <Route path='/' element={<CreateRoom/>}/>
         <Route path='/:id' element={<Room/>}/>
-        <Route path='*' element={<div><p>error page</p></div>}/>
+        <Route path='*' element={<div>
+          <p>error page</p>
+          <Navigate to={'/'}><p>go home</p></Navigate>
+        </div>}/>
       </Routes>
     </div>
   );
